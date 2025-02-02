2cf87df250dd71b2b9802703a0d61c85d988fde0
Tatiana Berezina <tatianaberezina1@yandex.ru>
2025-02-02 07:18:19 +0300

    changed hand check for assets

--- unit_testing_lesson1/tests/capitalize.test.js
+++ unit_testing_lesson1/tests/capitalize.test.js
@@ -1,11 +1,7 @@
+import { strict as assert } from 'assert';
 import capitalize from '../src/capitalize.js';
 
-if (capitalize('hello') !== 'Hello') {
-  throw new Error('Функция работает неверно!');
-}
-
-if (capitalize('') !== '') {
-  throw new Error('Функция работает неверно!');
-}
+assert(capitalize('') === '');
+assert(capitalize('hello') === 'Hello');
 
 console.log('Все тесты пройдены!');
